c600e8a0ca029ce81463e922fc713461426b2dd3
EmilAvramov <e_avramov@zoho.eu>
2024-06-29 15:27:29 +0300

    add search form to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { SearchForm } from '../forms/SearchForm'
 
 export const Header = (): JSX.Element => {
   return (
     <header>
+      <div>
+        <SearchForm />
+      </div>
       <nav>
         <ul>
           <li>
