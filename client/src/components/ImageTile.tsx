f48ff3944206116bfd44d9391d34c1f438856c9d
EmilAvramov <e_avramov@zoho.eu>
2024-06-29 15:26:35 +0300

    add image tile edit and delete functionality

--- client/src/components/ImageTile.tsx
+++ client/src/components/ImageTile.tsx
@@ -1,14 +1,36 @@
 import React from 'react'
 import type { Image } from '../types/image'
+import { useImagesAPI } from '../context/useImagesAPI'
+import { useNavigate } from 'react-router-dom'
 
 interface ImageTileProps {
   image: Image
 }
 
 export const ImageTile: React.FC<ImageTileProps> = ({ image }): JSX.Element => {
+  const { api, setImageData } = useImagesAPI()
+  const navigate = useNavigate()
+
+  const handleDelete = async () => {
+    const deleteResult = await api?.deleteImage({ id: image.id })
+    console.log(deleteResult)
+    if (deleteResult?.status === 204) {
+      const refetchedImages = await api?.getImages()
+      if (refetchedImages?.status === 200) {
+        setImageData(refetchedImages?.data)
+      }
+    }
+  }
+
+  const handleEdit = () => {
+    navigate('/edit', { state: { id: image.id } })
+  }
+
   return (
     <div>
       <div>{image.description}</div>
+      <button onClick={() => handleEdit()}>Edit</button>
+      <button onClick={() => handleDelete()}>Delete</button>
       <img src={image.url} alt={image.title} />
     </div>
   )
